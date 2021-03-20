171da8b09a2eb384f4acc03a3de7ccef46b59cb6
CarbonPhosphate <bwharton2003@gmail.com>
2021-03-20 23:11:55 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# CarbonPhosphate Mod Template
---
+++ src/main/java/net/carbonphosphate/more_pies/MorePiesMod.java
@@ -0,0 +1,13 @@
+package net.carbonphosphate.more_pies;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MorePiesMod implements ModInitializer {
+
+
+	@Override
+	public void onInitialize()
+	{
+		
+	}
+}
